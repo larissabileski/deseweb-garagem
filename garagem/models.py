9224de0a657a97166547e9f129087e67f9696476
larissabileski <laricristinabileski@gmail.com>
2023-03-21 15:13:57 -0300

    Adicionado Acessorios, Cores e Veiculos

--- garagem/models.py
+++ garagem/models.py
@@ -1,13 +1,49 @@
 from django.db import models
 
+
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+
     def __str__(self):
         return self.nome.upper()
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+
     def __str__(self):
         return self.descricao
 
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+
+class Veiculo(models.Model):
+    modelo = models.CharField(max_length=255, default='')
+    marca = models.ForeignKey(
+        Marca, on_delete=models.PROTECT, related_name="veiculo"
+    )
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="veiculo"
+    )
+    cor = models.ForeignKey(
+        Cor, on_delete=models.PROTECT, related_name="veiculo"
+    )
+    ano = models.IntegerField(default=0)
+
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.modelo} (Marca: {self.marca} - Ano: {self.ano} - Cor: {self.cor})"
