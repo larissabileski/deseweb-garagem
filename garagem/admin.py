88f78f5b7b75cfe0b503a359ab322be170ae321f
larissabileski <laricristinabileski@gmail.com>
2023-08-22 13:23:54 -0300

    Criação da entidade Compra integrada ao usuário

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo, Compra
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
 admin.site.register(Cor)
 admin.site.register(Veiculo)
+admin.site.register(Compra)
--- garagem/models/__init__.py
+++ garagem/models/__init__.py
@@ -3,3 +3,4 @@ from .categoria import Categoria
 from .acessorio import Acessorio
 from .cor import Cor
 from .veiculo import Veiculo
+from .compra import Compra
