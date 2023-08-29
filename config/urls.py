ca7d2b5eba2f52fbf82b858a9ce0a39cea00d9c1
larissabileski <laricristinabileski@gmail.com>
2023-08-29 14:12:19 -0300

    Correção do prof. Marco André

--- config/urls.py
+++ config/urls.py
@@ -24,12 +24,12 @@ from drf_spectacular.views import (
 )
 
 router = DefaultRouter()
-router.register(r"categorias", CategoriaViewSet)
-router.register(r"marcas", MarcaViewSet)
 router.register(r"acessorios", AcessorioViewSet)
+router.register(r"categorias", CategoriaViewSet)
 router.register(r"cores", CorViewSet)
-router.register(r"veiculos", VeiculoViewSet)
+router.register(r"marcas", MarcaViewSet)
 router.register(r"modelos", ModeloViewSet)
+router.register(r"veiculos", VeiculoViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
